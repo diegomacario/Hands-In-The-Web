b66254fd40f97d2b76270a4e72cff92c5cb9f085
diegomacario <diego.macario5@gmail.com>
2023-01-13 15:05:38 -0500

    Remove unnecessary emscripten includes.

--- inc/pch.h
+++ inc/pch.h
@@ -6,10 +6,4 @@
 #include <Alembic/AbcCoreOgawa/All.h>
 #include <Alembic/AbcGeom/All.h>
 
-#ifdef __EMSCRIPTEN__
-    #include <emscripten.h>
-    #include <emscripten/bind.h>
-    #define wabcAPI EMSCRIPTEN_KEEPALIVE extern "C" __attribute__((visibility("default")))
-#endif
-
 #endif
