4ac3636d4e25fd207e4b5dc5ba1cbb424e665d07
diegomacario <diego.macario5@gmail.com>
2023-01-16 17:56:59 -0500

    Remove lots of unnecessary things.

--- inc/GLTFLoader.h
+++ inc/GLTFLoader.h
@@ -6,12 +6,10 @@
 #include "cgltf/cgltf.h"
 
 #include "StaticMesh.h"
-#include "SimpleMesh.h"
 
 cgltf_data*               LoadGLTFFile(const char* path);
 void                      FreeGLTFFile(cgltf_data* handle);
 
 std::vector<StaticMesh>   LoadStaticMeshes(cgltf_data* data);
-std::vector<SimpleMesh>   LoadSimpleMeshes(cgltf_data* data);
 
 #endif
